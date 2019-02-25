4af635c0294cdbe37da2a431e5bcc384f2c536c6
Mateusz Kubaczyk <mkubaczyk@egnyte.com>
2019-02-25 09:41:08 +0100

    Modify script to use everything from brew instead of installing it manually

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-GCLOUD_VERSION="230.0.0"
-TERRAFORM_VERSION="0.11.11"
-MINIKUBE_VERSION="0.33.1"
 BINS_DIR="$HOME/bins"
+
+pwd=$(pwd)
 
 mkdir -p $HOME/bins
 mkdir -p $HOME/git/go/
@@ -17,19 +16,19 @@ cat <<EOF > $HOME/.bash_profile
 ############
 # env vars #
 ############
-export PATH="\$PATH:$BINS_DIR:/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+export PATH="/usr/local/opt/gnu-getopt/bin:\$PATH:$BINS_DIR:/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 export GOPATH=\$HOME/git/go
 
 ##########
 # gcloud #
 ##########
-export PATH="\$PATH:\$HOME/bins/google-cloud-sdk/bin"
-if [ -f "\$HOME/bins/google-cloud-sdk/completion.zsh.inc" ]; then . "\$HOME/bins/google-cloud-sdk/completion.zsh.inc"; fi
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 
 ###########
 # kubectx #
 ###########
-source \$ZSH/plugins/kube-ps1/kube-ps1.plugin.zsh 
+source \$ZSH/plugins/kube-ps1/kube-ps1.plugin.zsh
 PROMPT='\$(kube_ps1)'\$PROMPT
 
 ###########
@@ -38,6 +37,11 @@ PROMPT='\$(kube_ps1)'\$PROMPT
 alias kctx=kctx
 kctx() {
 	kubectx \$1
+}
+
+alias sshc=sshc
+sshc() {
+    cat $HOME/.ssh/config
 }
 
 EOF
@@ -54,63 +58,47 @@ EOF
 ########
 # bins #
 ########
-files=( tfplan )
+files=( )
 for filename in "${files[@]}"
 do
     curl -o $HOME/bins/$filename https://raw.githubusercontent.com/mkubaczyk/zsh-setup/master/bins/$filename
 done
 
+git clone git@github.com:mkubaczyk/tfplan.git $HOME/git/tfplan || true
+ln -fs $PWD/tfplan /usr/local/bin/tfplan || true
+ln -fs $PWD/tfapply /usr/local/bin/tfapply || true
+
 ########
 # brew #
 ########
 printf 'y\ny\n' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew_upgradable=( wget watch git git-crypt pwgen jq telnet zsh tmux dep kubectx fzf unzip gnu-getopt tree terraform_landscape terraform docker-machine-driver-hyperkit )
+brew_unupgradable=( kubernetes-helm kubernetes-cli go )
+brew_all=("${brew_upgradable[@]}" "${brew_unupgradable[@]}")
+brew_casks=( minikube google-cloud-sdk )
+brew tap caskroom/cask
 brew update
-set +e
-brew install wget watch git kubernetes-helm kubernetes-cli git-crypt pwgen jq telnet zsh tmux go dep kubectx fzf unzip
-set -e
+brew cask install "${brew_casks[@]}" || brew cask upgrade "${brew_casks[@]}" || true
+brew install "${brew_all[@]}" || brew upgrade "${brew_upgradable[@]}" || true
 
-#######
-# zsh #
-#######
-if ! grep -q "source \$HOME/.bash_profile" $HOME/.zshrc; then
-	echo "source \$HOME/.bash_profile" >> $HOME/.zshrc
-fi
+sudo chown root:wheel /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+sudo chmod u+s /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+
+helm init --client-only
+helm plugin install https://github.com/futuresimple/helm-secrets || helm plugin update https://github.com/futuresimple/helm-secrets || true
 
 #############
 # oh my zsh #
 #############
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm -rf $HOME/.oh-my-zsh/
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash - || true
 
-##########
-# gcloud #
-##########
-rm -f "/tmp/google-cloud-sdk-$GCLOUD_VERSION-darwin-x86_64.tar.gz"
-wget -N -P /tmp/ "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$GCLOUD_VERSION-darwin-x86_64.tar.gz"
-tar -xzf "/tmp/google-cloud-sdk-$GCLOUD_VERSION-darwin-x86_64.tar.gz" -C $BINS_DIR
-rm -f "/tmp/google-cloud-sdk-$GCLOUD_VERSION-darwin-x86_64.tar.gz"
-printf 'y\ny\n' | sh $BINS_DIR/google-cloud-sdk/install.sh
-
-#############
-# terraform #
-#############
-rm -f /tmp/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip
-wget -N -P /tmp/ "https://releases.hashicorp.com/terraform/0.11.11/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip"
-unzip -o "/tmp/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip" -d $BINS_DIR
-rm -f /tmp/terraform_${TERRAFORM_VERSION}_darwin_amd64.zip
-chmod +x $BINS_DIR/terraform
-set +e
-brew install terraform_landscape
-set -e
-
-############
-# minikube #
-############
-rm -f /tmp/minikube-darwin-amd64
-wget -N -P /tmp/ "https://github.com/kubernetes/minikube/releases/download/v$MINIKUBE_VERSION/minikube-darwin-amd64"
-mv /tmp/minikube-darwin-amd64 $BINS_DIR/minikube
-chmod +x $BINS_DIR/minikube
-
-curl -o /tmp/docker-machine-driver-hyperkit -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit
-sudo install -o root -g wheel -m 4755 /tmp/docker-machine-driver-hyperkit $BINS_DIR/
-rm /tmp/docker-machine-driver-hyperkit
-
+#######
+# zsh #
+#######
+if ! grep -q "export ZSH_DISABLE_COMPFIX=true" $HOME/.zshrc; then
+    echo "export ZSH_DISABLE_COMPFIX=true" | cat - $HOME/.zshrc > /tmp/.zshrc && mv /tmp/.zshrc $HOME/.zshrc
+fi
+if ! grep -q "source \$HOME/.bash_profile" $HOME/.zshrc; then
+	echo "source \$HOME/.bash_profile" >> $HOME/.zshrc
+fi
